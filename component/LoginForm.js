5ee15e1f2f0a5d17866e0b684e0bc0ad257b5f43
Ammar Ahmed <ammar69aa59@gmail.com>
2024-09-08 00:22:45 -0700

    inpu-feilds validations added

--- component/LoginForm.js
+++ component/LoginForm.js
@@ -1,35 +1,104 @@
-import { View, TextInput, StyleSheet, Text } from "react-native";
+import { View, TextInput, StyleSheet, Text, Pressable } from "react-native";
 import React from "react";
 import { Button, Divider } from "react-native-elements";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import Validator from "email-validator";
 
 const LoginForm = () => {
+  const loginSchema = Yup.object().shape({
+    email: Yup.string().email("Invalid email").required("Required"),
+    password: Yup.string()
+      .required("Required")
+      .min(8, "Your password has to have at least 8 characters"),
+  });
+
   return (
     <View style={styles.wrapper}>
-      <View style={styles.inputField}>
-        <TextInput
-          placeholder="Email or Phone Number"
-          placeholderTextColor="#444"
-          autoCapitalize="none"
-          keyboardType="email-address"
-          textContentType="emailAddress"
-          autoFocus={true}
-          style={styles.textInput}
-        />
-        <Divider width={1} orientation="horizontal" />
-        <TextInput
-          placeholder="Password"
-          placeholderTextColor="#444"
-          autoCapitalize="none"
-          autoCorrect={false}
-          secureTextEntry={true}
-          textContentType="password"
-          style={styles.textInput}
-        />
-      </View>
-      <View style={{ alignItems: "flex-end", marginBottom: 30 }}>
-        <Text>Forget Password ?</Text>
-      </View>
-      <Button title="Log In" buttonStyle={styles.loginButton} />
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validationSchema={loginSchema}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          errors,
+          touched,
+        }) => (
+          <>
+            {/* Email Input */}
+            <View
+              style={[
+                styles.inputField,
+                {
+                  borderColor:
+                    values.email.length < 1 || Validator.validate(values.email)
+                      ? "#ccc"
+                      : "red",
+                },
+              ]}
+            >
+              <TextInput
+                placeholder="Email or Phone Number"
+                placeholderTextColor="#444"
+                autoCapitalize="none"
+                keyboardType="email-address"
+                textContentType="emailAddress"
+                autoFocus={true}
+                style={styles.textInput}
+                onChangeText={handleChange("email")}
+                onBlur={handleBlur("email")}
+                value={values.email}
+              />
+            </View>
+
+            {/* Password Input */}
+            <View
+              style={[
+                styles.inputField,
+                {
+                  borderColor: values.password.length >= 8 ? "#ccc" : "red",
+                },
+              ]}
+            >
+              <TextInput
+                placeholder="Password"
+                placeholderTextColor="#444"
+                autoCapitalize="none"
+                autoCorrect={false}
+                secureTextEntry={true}
+                textContentType="password"
+                style={styles.textInput}
+                onChangeText={handleChange("password")}
+                onBlur={handleBlur("password")}
+                value={values.password}
+              />
+            </View>
+
+            <View style={{ alignItems: "flex-end", marginBottom: 30 }}>
+              <Text>Forget Password?</Text>
+            </View>
+
+            <Button
+              title="Log In"
+              buttonStyle={styles.loginButton}
+              onPress={handleSubmit}
+            />
+
+            <View style={styles.signUpContainer}>
+              <Text style={{ fontWeight: "600" }}>Don't have an account?</Text>
+              <Pressable onPress={() => console.log("Sign Up button clicked")}>
+                <Text style={{ color: "#0096F6" }}>Sign Up</Text>
+              </Pressable>
+            </View>
+          </>
+        )}
+      </Formik>
     </View>
   );
 };
@@ -41,11 +110,13 @@ const styles = StyleSheet.create({
   },
   inputField: {
     backgroundColor: "#FAFAFA",
-    padding: 8,
+    flexDirection: "row",
+    alignItems: "center",
+    // justifyContent:"center",
     borderWidth: 1,
-    borderColor: "#ddd",
     borderRadius: 4,
     marginBottom: 10,
+    padding: 5,
   },
   textInput: {
     height: 40,
@@ -55,6 +126,18 @@ const styles = StyleSheet.create({
     backgroundColor: "#0096F6",
     borderRadius: 5,
     height: 45,
+  },
+  signUpContainer: {
+    flexDirection: "row",
+    marginTop: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  errorText: {
+    color: "red",
+    fontSize: 12,
+    marginTop: -5,
+    marginBottom: 5,
   },
 });
 
